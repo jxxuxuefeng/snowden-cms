a3d01495b8b01e9de8e7a19cc1be6b44549fb66d
snowden.xu <snowden.xu@gmail.com>
2024-06-19 14:04:04 +0800

    feat: 登录页完善

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,91 +7,106 @@ import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import Image from "next/image";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type FormData = {
-  username: string;
-  password: string;
-};
+const FormSchema = z.object({
+  username: z.string().min(1, {
+    message: "用户名不能为空",
+  }),
+  password: z.string().min(1, {
+    message: "密码不能为空",
+  }),
+});
 
 export default function Home() {
-  const form = useForm();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
   const onSubmit = () => {
-    console.log(form.getValues());
+    console.log(form.getValues(), "form");
   };
 
   return (
-    <div className="w-full h-screen bg-gray-100">
-      <main className="px-8 py-16 flex flex-col items-center">
+    <div className="w-full h-screen bg-gray-100 bg-[url('/login-bg.png')] [background-size:100%_100%]">
+      <main className="px-8 pt-16 flex flex-col items-center">
         <div className="w-[552px] px-14 py-12 border bg-white shadow">
           <div className="flex items-center flex-col">
-            <img
-              src={"logo.svg"}
-              className="w-[4.5rem] pb-3"
+            <Image
+              src={"/logo.png"}
+              className="pb-3"
               title="logo"
+              width={72}
+              height={72}
               alt="logo"
             />
-            <div className="text-2xl pb-2 font-bold">欢迎使用 snowden CMS</div>
-            <div className="text-sm pb-2 text-gray-600">登录您的账户</div>
+            <div className="text-2xl py-6 font-bold">欢迎使用 snowden CMS</div>
           </div>
           <div className="w-full flex flex-col space-y-6">
-            <div className="space-y-2">
+            <div className="space-y-6">
               <Form {...form}>
                 <FormField
                   control={form.control}
+                  name="username"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel className="flex items-center font-[600] text-[0.75rem]">
                         用户名<span className="text-red-700 text-sm">*</span>
                       </FormLabel>
                       <FormControl>
-                        <Input placeholder="例如: admin" {...field} />
+                        <Input placeholder="请输入用户名" {...field} />
                       </FormControl>
-                      <FormMessage />
+                      <FormMessage className="absolute text-xs font-normal" />
                     </FormItem>
                   )}
-                  name={"username"}
                 />
                 <FormField
                   control={form.control}
+                  name="password"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel className="flex items-center font-[600] text-[0.75rem]">
                         密码<span className="text-red-700 text-sm">*</span>
                       </FormLabel>
                       <FormControl>
-                        <Input type="password" {...field} />
+                        <Input
+                          placeholder="请输入密码"
+                          type="password"
+                          {...field}
+                        />
                       </FormControl>
-                      {/*<FormDescription>des</FormDescription>*/}
-                      <FormMessage />
+                      <FormMessage className="absolute text-xs font-normal" />
                     </FormItem>
                   )}
-                  name={"password"}
                 />
               </Form>
             </div>
-            <div className="flex items-center" space-x-2>
-              <Checkbox id="terms" />
-              <label htmlFor="terms" className="cursor-pointer">
-                记住我
-              </label>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-2">
+                <Checkbox id="terms" />
+                <label htmlFor="terms" className="cursor-pointer">
+                  自动登录
+                </label>
+              </div>
+              <Button variant="link" className="px-0">
+                <Link href="#">忘记密码？</Link>
+              </Button>
             </div>
-            <Button size="sm" onClick={onSubmit}>
-              登录
-            </Button>
+            <Button onClick={form.handleSubmit(onSubmit)}>登 录</Button>
           </div>
         </div>
-        <div className="pt-4">
-          <Button variant="link">
-            <Link href="#">忘记密码？</Link>
-          </Button>
-        </div>
+        <div className="mt-16 text-gray-400">&copy; Powered by snowden.xu</div>
       </main>
     </div>
   );
